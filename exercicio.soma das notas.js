ab83823ecd733f837cf60d3931dfa9c12570d8e1
Ana Livia <analivnc@gmail.com>
2025-05-23 11:33:03 -0300

    Add files via upload
    Primeiro exercicío em JavaScript na Bosch.

---
+++ exercicio.soma das notas.js
@@ -0,0 +1,14 @@
+entrada = require('prompt-sync')();
+
+//entrada de dados
+let nota1 = Number(entrada("digite a primeira nota: "))
+let nota2 = Number(entrada("digite a segunda nota: "))
+let nota3 = Number(entrada("digite a terceira nota: "))
+let nota4= Number(entrada("digite a quarta nota: "))
+
+//processamento de dados
+let media = (nota1 + nota2 + nota3 + nota4)
+
+//saída de dados
+console.log('a media é ', media);
+

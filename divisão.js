c65856e6140704ca535dcb36e04fc72aa0cdbfec
Ana Livia <analivnc@gmail.com>
2025-05-26 10:39:23 -0300

    Add files via upload

---
+++ divisão.js
@@ -0,0 +1,8 @@
+let numero1 = 14;
+let numero2 = 4;
+//divisao normal;
+console.log(numero1 / numero2);
+//resto da divisão
+console.log(numero1 % numero2);
+//parte inteira da divisão
+console.log(Math.floor(numero1/numero2));
